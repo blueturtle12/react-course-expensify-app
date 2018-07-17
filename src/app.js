385a6d92228087bb6cf8c2d54993d50f028fdff8
hachem jadaoui <blueturtle1293@gmail.com>
2018-07-18 00:07:17 +0300

    add styling

--- src/app.js
+++ src/app.js
@@ -10,25 +10,23 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
 let hasRendered = false;
 const renderApp = () => {
-  if(!hasRendered) {
+  if (!hasRendered) {
     ReactDOM.render(jsx, document.getElementById('app'));
     hasRendered = true;
   }
 };
 
-ReactDOM.render(<p>loading ...</p>, document.getElementById('app'));
-
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
@@ -38,9 +36,9 @@ firebase.auth().onAuthStateChanged((user) => {
       if (history.location.pathname === '/') {
         history.push('/dashboard');
       }
-    });    
+    });
   } else {
-    store.dispatch(logout);
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
